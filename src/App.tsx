c6686c2cc6fb65782c9b41114c5382f6823d510d
jackykwandesign <jackykwandesign@gmail.com>
2022-06-07 06:52:14 +0000

    v2

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Test AWS amplify 
         </a>
       </header>
     </div>
